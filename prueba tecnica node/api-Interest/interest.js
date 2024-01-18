e076ccd175a217133e2bc341f96f2c23eb6c0a7c
nikolas2603843 <nabermudez98@misena.edu.co>
2024-01-18 12:36:20 -0500

    comentarios

--- prueba tecnica node/api-Interest/interest.js
+++ prueba tecnica node/api-Interest/interest.js
@@ -1,13 +1,28 @@
+/**
+ * para las rutas y poder darle funcionalidad a los servicios
+ */
 const express = require('express');
+/**
+ * obtiene la informacion de la pagina web
+ */
 const axios = require('axios');
+/**
+ * el que nos ayuda con el web scraping
+ */
 const cheerio = require('cheerio');
 
 const router = express.Router();
 
 router.get('/interest/:plazo/:monto', async (req, res) => {
     try {
+        /**
+         * optiene los path params 
+         */
         const plazo = req.params.plazo;
         const monto = req.params.monto;
+        /**
+         * web scaping
+         */
         const link = 'https://www.larepublica.co/indicadores-economicos/bancos/tasa-de-usura';
         const response = await axios.get(link);
         const infoHtml = response.data;
@@ -16,15 +31,24 @@ router.get('/interest/:plazo/:monto', async (req, res) => {
         const usuryText = idEtiquetaPrincipal.text().trim();
         const usuryNumber = usuryText.replace(',', '.').replace('%', '');
         const rate = parseFloat(usuryNumber);
+        /**
+         * objeto json a retornar
+         */
         const interestJSON = {
             usuryRate: (monto*rate*plazo)/100,
             rate:rate
         }
+        /**
+         * retorno de objeto json
+         */
         return res.status(200).json(interestJSON)
     } catch (error) {
         return res.status(500).json({ error: 'Todo ha fallado exitosamente' });
     }
 });
 
+/**
+ * exportacion del modulo
+ */
 module.exports = router;
 
--- prueba tecnica node/main.js
+++ prueba tecnica node/main.js
@@ -5,8 +5,17 @@ const appExpress = express();
 const port = 3000;
 const interest = require('./api-Interest/interest');
 
+/**
+ * cors para seguridad 
+ */
 appExpress.use(cors());
+/**
+ * ayuda a extraer la data del cuerpo en la peticion http
+ */
 appExpress.use(bodyParser.json());
+/**
+ * hace uso del api de interest para que pueda ser usada
+ */
 appExpress.use('/api',interest);
 
 
